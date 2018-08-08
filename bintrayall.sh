a4f0918f4cf6081fffe318d3462aad258533d7ef
Kevin Galligan <kgalligan@gmail.com>
2018-08-08 06:06:39 -0400

    Konan gradle version

--- bintrayall.sh
+++ bintrayall.sh
@@ -1,4 +1,13 @@
-./gradlew bintrayUpload -PdeployIosRuntime=true
-./gradlew bintrayUpload -PdeployCommon=true
-./gradlew bintrayUpload -PdeployIosDriver=true
+set -e
+./gradlew bintrayUpload -PdeployCore=true
 
+./gradlew bintrayUpload -PdeployGradle=true
+./gradlew bintrayUpload -PdeployMigrations=true
+
+./gradlew bintrayUpload -PdeployRuntimeCommon=true
+./gradlew bintrayUpload -PdeployRuntimeNative=true
+./gradlew bintrayUpload -PdeployRuntimeJdk=true
+
+./gradlew bintrayUpload -PdeployMultiplatformCommon=true
+./gradlew bintrayUpload -PdeployMultiplatformIos=true
+./gradlew bintrayUpload -PdeployMultiplatformAndroid=true
